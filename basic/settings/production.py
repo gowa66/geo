019cbe80891afbe57d07678c98c74b214198242d
gowa66 <gowa66at@gmail.com>
2016-05-13 11:04:16 +0300

    init

---
+++ basic/settings/production.py
@@ -0,0 +1,25 @@
+import os
+import dj_database_url
+from django.conf import settings
+
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+DATABASES = settings.DATABASES
+
+# Parse database configuration from $DATABASE_URL
+DATABASES['default'] =  dj_database_url.config()
+
+# Enable Persistent Connections
+DATABASES['default']['CONN_MAX_AGE'] = 500
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
